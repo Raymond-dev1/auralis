5af8b7114d14ac961c122c7e312f610eefcda926
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-22 13:13:54 +0100

    feat: implement navbar design

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
-import './App.css'
-import NavBar from './components/NavBar'
-
+import "./App.css";
+import NavBar from "./components/NavBar";
 
 const navlinks = [
-  {label: 'Home', href: '/'},
-  {label: 'About', href: '/about'}
-]
+  { label: "Auralis Studio", href: "/" },
+  { label: "Auralis Agents", href: "/agent" },
+  { label: "Auralis API", href: "/api" },
+  { label: "Resources", href: "/resource" },
+  { label: "Enteprise", href: "/enteprise" },
+  { label: "Pricing", href: "/pricing"},
+];
 
 function App() {
-  return <NavBar links= {navlinks} />
+  return (
+    <div className="min-h-screen bg-[F3F2EF]">
+  <NavBar links={navlinks} />
+  </div>
+  )
 }
 
-
-export default App
+export default App;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,31 +1,40 @@
-
+// import {
+//   Card,
+//   CardContent,
+//   CardDescription,
+//   CardHeader,
+//   CardTitle,
+// } from "@/components/ui/card";
 
 type NavLink = {
-    label: string;
-    href: string;
-}
+  label: string;
+  href: string;
+};
 
 type NavBarProps = {
-    links: NavLink[];
-    logoText? :string;
-}
+  links: NavLink[];
+  logoText?: string;
+};
 
-
-export default function NavBar({links, logoText = 'Auralis' }: NavBarProps) {
+export default function NavBar({ links, logoText = "Auralis" }: NavBarProps) {
   return (
-    <nav className="navbar  flex justify-center sticky top-0 right-0 bg-amber-400">
-      <div className="text-lg font-semibold bg-amber-950 ">{logoText}</div>
-
-      <ul className="flex items-center gap-6">
-        {links.map((link:any) => (
-          <li key={link.href}>
-            <a href={link.href} className="text-sm hover:opacity-70">
-              {link.label}
-            </a>
-          </li>
-        ))}
-      </ul>
+    <nav className="navbar font-geist sticky top-0 z-50 flex items-center justify-between px-6 py-4 h-21 border-b bg-background">
+      <div className="flex gap-20">
+      <div className="flex items-center  gap-4">
+      <span className="font-extrabold text-lg text-[#18181B]">{logoText}</span>
+      </div>
+        <ul className="flex items-center gap-6 text-[#71717A]">
+          {links.map((link: NavLink) => (
+            <li key={link.href}>
+              <a href={link.href} className="text-medium font-medium opacity-100">
+                {link.label}
+              </a>
+            </li> 
+          ))}
+        </ul>
+        </div>
+        {/* CTA */}
+        <button className="items-center bg-black text-sm text-white font-medium px-3 h-9 rounded-2xl"> Sign up</button>
     </nav>
   );
 }
- 
