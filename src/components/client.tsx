362f988552b42d5a6982903895ce9f212a7a2940
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-23 18:18:45 +0100

    feat: implement client section design

---
+++ src/components/client.tsx
@@ -0,0 +1,13 @@
+
+
+export default function client(){
+    return(
+    <section>
+        <div className="font-sans justify-center h-38 flex  m-6">
+        <div className="flex items-center">
+            <span className="text-[#71717A] font-semibold text-sm pt-35  tracking-[1.2px]">TRUSTED BY TEAMS BUILDING MODERN EXPERIENCES</span>
+        </div>
+        </div>
+    </section>
+    )
+}
