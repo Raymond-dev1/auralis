cb9c8dcff78f80d3c5a8c145518c76eccf86d8e6
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-07-26 23:28:18 +0100

    feat: add logo for each company listed

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,12 +1,27 @@
-const logos = ["VELOCITY", "NEXUS", "LUMINA", "STACK", "QUANTA", "ORBITAL"];
+import velLogoUrl from "../assets/vel-logo.svg"
+import nexusLogoUrl from "../assets/nexus-logo.svg"
+import luminaLogoUrl from "../assets/lumina-logo.svg"
+import stackLogoUrl from "../assets/stack-logo.svg"
+import quantaLogoUrl from "../assets/quanta-logo.svg"
+import orbitalLogoUrl from "../assets/orbital-logo.svg"
+
+const logos =[
+  {name: "VELOCITY",  src : velLogoUrl },
+  {name: "NEXUS",  src : nexusLogoUrl  },
+  {name: "LUMINA",  src : luminaLogoUrl},
+  {name: "STACK",  src : stackLogoUrl },
+  {name: "QUANTA",  src : quantaLogoUrl },
+  {name: "ORBITAL",  src : orbitalLogoUrl }
+]
 
 export default function LogoMarquee() {
   return (
     <div className="py-4 scroll-fade-x overflow-hidden">
       <div className="flex  w-max animate-marquee">
         {[...logos, ...logos].map((logo, i) => (
-          <span key={i} className="font-bold px-13 shrink-0">
-            {logo}
+          <span key={i} className="font-bold flex items-center gap-2 px-13 shrink-0">
+            <img src={logo.src} alt= {logo.name} className="h-6"/>
+            {logo.name}
           </span>
         ))}
       </div>
