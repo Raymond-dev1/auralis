23e99052f6eeaca8fa3c15fa9c2ec4e891d0d3ec
Raymond-dev1 <iziogbaraymond72@gmail.com>
2026-08-06 19:05:13 +0100

    fix: add responsiveness to footer section

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,10 +3,10 @@ const socialLinks =["Twitter", "Github", "Terms", "Privacy"]
 
 export default function Footer() {
   return (
-    <section className="font-sans">
-      <div className="w-full flex  flex-col gap-8 items-center pb-47 mt-10">
-        <span className="pt-25">
-          <h2 className="text-4xl tracking-normal">
+    <section className="font-sans overflow-hidden">
+      <div className="w-full flex flex-col gap-8 items-center pb-47 mt-10">
+        <span className="pt-185 sm:pt-25 md:pt-25">
+          <h2 className="text-[20px] md:text-4xl tracking-normal">
             {" "}
             The Future of voice starts here
           </h2>
@@ -27,23 +27,20 @@ export default function Footer() {
         </div>
       </div>
       {/* Footer */}
-      <div className="px-12 py-12 w-full">
-        <div className="flex items-center gap-120 w-full ">
+      <div className="md:px-12 md:py-12 p-8 w-full">
+        <div className="flex items-center justify-between  w-full ">
         <div className="flex flex-row gap-8 items-center p-3">
-          <div className="font-extrabold text-lg tracking-[-0.9px] text-[#18181B]">
+          <div className="font-extrabold text-[10px]  sm:text-lg tracking-[-0.9px] text-[#18181B]">
             Auralis 
           </div>
-          <div>
-            <p className="text-[#71717A] text-sm font-light">@ 2024 Auralis AI.Engineered for precision.</p>
           </div>
-          </div>
-        <div>
-          <ul className="flex-row flex gap-8 ">
+        <div className="">
+          <ul className="md:flex-row flex gap-4 md:gap-8 ">
             {socialLinks.map((link, i) => (
-                <li key={i} className="text-[#6B6B6B]  font-normal">
+                <li key={i} className="text-[#6B6B6B]  text-[10px] sm:text-sm md:font-normal">
                   {link}
               </li>
-))} 
+        ))} 
           </ul>
         </div>
         </div>
